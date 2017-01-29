3b58d0921bec473abe99c59824b3d95f5e567511
Ron <ron.suprun@gmail.com>
2017-01-29 15:47:45 -0500

    Fixed routing issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  root render html: "Hello World"
+
+  def hello
+   render html: "Hello World"
+  end
+
 end
